b569f305b4e149c28ca80499cf662b1381ad9ed3
Marina Chimeno <chimenomarina@gmail.com>
2025-05-02 20:36:18 +0200

    Initial commit
    - Vehicle template.
    - Created own kart pawn, GoKart.
    - Implemented movement and rotation of the kart.
    - Enabled collisions.
    - Implemented air resistance.

---
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KRAZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	// Mass of the car (kg)
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+
+	// Force applied to the car when the throttle is fully down (N)
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	// Number of degrees rotated per second at full control throw (degrees/s)
+	UPROPERTY(EditAnywhere)
+	float MaxDegreesPerSecond = 90;
+
+	// Higher means more drag (Kg/m)
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16;
+		
+	float Throttle;
+
+	float SteeringThrow;
+	
+	FVector Velocity;
+
+	FVector GetResistance();
+	void ApplyRotation(float DeltaTime);
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+};
